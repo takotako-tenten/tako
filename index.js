ae030818c274a2e15c4808163cb308df9355d478
takoten <kirutezusuki@gmail.com>
2026-07-14 18:11:02 +0900

    ダークモードを作る途中

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
+const body = document.querySlector("#body");
+const title = document.querySelecor("#title");
 const text = document.querySelector("#text");
 const button = document.querySelector("#button");
-let color = 0;
-
-button.addEventListener("click",function() {
-    if (color === 1) {
-        text.classList.remove("red");
-        text.classList.add("blue");
-        color = 2;
-    } else if (color === 2) {
-        text.classList.remove("blue");
-        text.classList.add("red");
-        color = 1;
-    } else {
-        text.classList.add("blue");
-        color = 2;
-    }
-});
