40007a2d1f81af08a1604695323ed936246a0d3e
takoten <kirutezusuki@gmail.com>
2026-07-17 15:22:29 +0900

    ダークモードを習得

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const body = document.querySlector("#body");
-const title = document.querySelecor("#title");
+const body = document.querySelector("#body");
+const title = document.querySelector("#title");
 const text = document.querySelector("#text");
 const button = document.querySelector("#button");
+
+button.addEventListener("click",function() {
+    body.classList.toggle("dark");
+});
